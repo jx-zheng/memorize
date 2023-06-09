d7fa4f329e3eaf2d9d50f216bbd70c51e2672c9c
Kevin Zheng <jxzheng@uwaterloo.ca>
2023-06-09 14:49:27 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kevin Zheng on 2023-06-09.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
