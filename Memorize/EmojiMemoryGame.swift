d7585e01b608c4beed88541c10667e94487984e5
Kevin Zheng <jxzheng@uwaterloo.ca>
2023-06-16 04:21:35 -0400

    add game logic in model

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -7,10 +7,20 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
-    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4) { _ in "😌" }
+class EmojiMemoryGame: ObservableObject {
+    private static let emojis = ["🙂", "😀", "😄", "😍", "🤩", "😎", "😊", "😃", "😆", "😇", "😉", "😋", "😘", "😌", "😜", "😝"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in emojis[pairIndex] }
+    }
+    
+    @Published private var model: MemoryGame<String> = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
         return model.cards
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
     }
 }
